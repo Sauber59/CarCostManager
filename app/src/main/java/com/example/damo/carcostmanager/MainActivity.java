912d2215ae0f55d311d3bdeae5adf2df8587af07
Sauber59 <damianweg@gmail.com>
2019-01-06 01:31:15 +0100

    Dodanie LoginActivity

--- app/src/main/java/com/example/damo/carcostmanager/MainActivity.java
+++ app/src/main/java/com/example/damo/carcostmanager/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.damo.carcostmanager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,15 +18,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -48,5 +40,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void click(View view) {
+        Intent intent = null;
+
+        intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
